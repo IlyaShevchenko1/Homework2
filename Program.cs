6f08c0df1e0ecd970b48147175aff594215a29e0
Ilya Shevchenko <6BLK@mail.ru>
2023-07-30 00:28:05 +0300

    Second task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Homework 2
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+/*
+int DozenIndicator (int number)
+{
+    int lastTwoDigits = number % 100;
+    int secondDigit = lastTwoDigits / 10;
+    return secondDigit;
+}
+
+Console.Write("Input a number from 100 till 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = DozenIndicator(number);
+
+Console.Write($"Second digit of your number {number} is {secondDigit}");
+
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// ( или -1, если третьей цифры нет).
+
+/*
+
+int ThirdDigitOfNumber (int number)
+{
+    int res = 0;
+
+    if (number < 100 )
+    {
+        res = - 1;
+        return res;
+    }
+    else 
+    {
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+    res = number % 10;
+    return res;
+    }    
+}
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = ThirdDigitOfNumber(number);
+Console.WriteLine($"Third digit of the number {number} is {thirdDigit}");
+
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую
+// день недели, и проверяет, является ли этот день выходным.
+
+bool WeekendCheck (int dayNumber)
+{
+    return dayNumber = 6 || dayNumber = 7;
+     
+}
+
